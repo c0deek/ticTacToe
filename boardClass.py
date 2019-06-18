28219c6766ba764779da9ea8625ab9e6a28996ad
Prateek Tripathi <ptripathi@mt.iitr.ac.in>
2019-06-18 16:51:32 +0530

    Added winner() method which checks the winner and stops the game

--- boardClass.py
+++ boardClass.py
@@ -19,6 +19,9 @@ class ticTacToe:
 
 	def showBoard(self):
 		print(f"""
+		Player 1: 0
+		Player 2: X
+
 			{self.board[1]}|{self.board[2]}|{self.board[3]}
 			-----
 			{self.board[4]}|{self.board[5]}|{self.board[6]}
@@ -33,12 +36,31 @@ class ticTacToe:
 	def changePlayer(self):
 		self.player = self.players[self.player]
 
+	def winner(self):
+		if(
+			(self.board[1] == self.board[2] and self.board[1] == self.board[3]) or
+			(self.board[4] == self.board[5] and self.board[4] == self.board[6]) or
+			(self.board[7] == self.board[8] and self.board[7] == self.board[9]) or
+			(self.board[1] == self.board[4] and self.board[1] == self.board[7]) or
+			(self.board[2] == self.board[5] and self.board[2] == self.board[8]) or
+			(self.board[3] == self.board[6] and self.board[3] == self.board[9]) or
+			(self.board[1] == self.board[5] and self.board[1] == self.board[9]) or
+			(self.board[3] == self.board[5] and self.board[3] == self.board[7])
+		):
+			return True
+		else:
+			return False
+
+	def checkWinner(self):
+		if(self.winner()):
+			print(f"{self.players[self.player]} won")
+			return True
+		else:
+			return False
+
 	def start(self):
 		self.showBoard()
-		for i in range(1,10):
+		while(not self.checkWinner()):
 			self.move()
 			self.changePlayer()
 			self.showBoard()
-
-game = ticTacToe()
-game.start()
